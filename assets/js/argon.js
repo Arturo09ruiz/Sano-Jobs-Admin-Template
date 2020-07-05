9e595827a0cfd4eb5801042b1f23d55f17ab59dd
Gregory <gregory09ruiz@gmail.com>
2020-07-04 22:20:46 -0400

    SR_READY

--- assets/js/argon.js
+++ assets/js/argon.js
@@ -1086,3 +1086,142 @@ var Scrollbar = (function() {
 	}
 
 })();
+
+
+var ctx = document.getElementById('myChart');
+var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = $('#myChart');
+var ctx = 'myChart';
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Henry Silva', 'Pedro Perez', 'David Guetta', 'Jose Martinez', 'Aviici', 'Michael Jackson'],
+        datasets: [{
+            label: '# of Votes',
+            data: [20, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
+var ctx = document.getElementById('myChartCitis');
+var ctx = document.getElementById('myChartCitis').getContext('2d');
+var ctx = $('#myChartCitis');
+var ctx = 'myChartCitis';
+
+var ctx = document.getElementById('myChartCitis');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Ciudad Bolívar', 'Puerto Ordaz', 'Barcelona', 'Puerto la Cruz', 'Caracas', 'Valencia'],
+        datasets: [{
+            label: '# of Votes',
+            data: [2, 4, 6, 8, 10, 12],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
+
+
+var ctx = document.getElementById('myChartConsejo');
+var ctx = document.getElementById('myChartConsejo').getContext('2d');
+var ctx = $('#myChartConsejo');
+var ctx = 'myChartConsejo';
+
+var ctx = document.getElementById('myChartConsejo');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Barcelona', 'Maracaibo', 'Caracas', 'Valencia'],
+        datasets: [{
+            label: '# of Votes',
+            data: [2, 4, 6, 8,],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
